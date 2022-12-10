4c538dfec927ad5cc3228c95daf8cff38fbd356f
IrsalLaz <91246036+IrsalLaz@users.noreply.github.com>
2022-12-10 21:12:29 +0700

    fix login&register page;

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -31,25 +31,31 @@
             </div>
 
             <!-- Remember Me -->
-            <div class="block mt-4">
+            <div class="block mt-4 flex justify-between">
                 <label for="remember_me" class="inline-flex items-center">
                     <input id="remember_me" type="checkbox"
                         class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
-            </div>
 
-            <div class="flex items-center justify-end mt-4">
                 @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+                    <a class="text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                         href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
+            </div>
 
-                <x-primary-button class="ml-3">
+            <div class="mt-4 block">
+                <x-auth-button>
                     {{ __('Log in') }}
-                </x-primary-button>
+                </x-auth-button>
+
+                <a class="mt-2 w-full flex justify-end text-sm text-gray-600 hover:text-gray-900 rounded-md"
+                    href="{{ route('register') }}">
+
+                    {{ __('Not have account? Register here') }}
+                </a>
             </div>
         </form>
     </x-auth-card>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,14 +12,16 @@
             <!-- Name -->
             <div>
                 <x-input-label for="name" :value="__('Name')" />
-                <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus />
+                <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')"
+                    required autofocus />
                 <x-input-error :messages="$errors->get('name')" class="mt-2" />
             </div>
 
             <!-- Email Address -->
             <div class="mt-4">
                 <x-input-label for="email" :value="__('Email')" />
-                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required />
+                <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
+                    required />
                 <x-input-error :messages="$errors->get('email')" class="mt-2" />
             </div>
 
@@ -27,10 +29,8 @@
             <div class="mt-4">
                 <x-input-label for="password" :value="__('Password')" />
 
-                <x-text-input id="password" class="block mt-1 w-full"
-                                type="password"
-                                name="password"
-                                required autocomplete="new-password" />
+                <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required
+                    autocomplete="new-password" />
 
                 <x-input-error :messages="$errors->get('password')" class="mt-2" />
             </div>
@@ -39,21 +39,21 @@
             <div class="mt-4">
                 <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
 
-                <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required />
+                <x-text-input id="password_confirmation" class="block mt-1 w-full" type="password"
+                    name="password_confirmation" required />
 
                 <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
             </div>
 
-            <div class="flex items-center justify-end mt-4">
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                    {{ __('Already registered?') }}
-                </a>
-
-                <x-primary-button class="ml-4">
+            <div class="mt-4 justify-center">
+                <x-auth-button>
                     {{ __('Register') }}
-                </x-primary-button>
+                </x-auth-button>
+
+                <a class="mt-2 w-full flex justify-end text-sm text-gray-600 hover:text-gray-900 rounded-md"
+                    href="{{ route('login') }}">
+                    {{ __('Already registered? Login Here') }}
+                </a>
             </div>
         </form>
     </x-auth-card>
