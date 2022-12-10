85695c1eb31da77b570e2e279cf2b9d893716877
IrsalLaz <91246036+IrsalLaz@users.noreply.github.com>
2022-12-11 00:07:26 +0700

    pull from panji

---
+++ resources/views/tagsPage.blade.php
@@ -0,0 +1,9 @@
+<x-app-layout>
+    <x-slot name="header">
+        <div class="">
+            <h3 class="rounded-md bg-white p-2 font-semibold text-cyan-800 leading-tight ml-2 flex justify-start">
+                # Pakan Ternak
+            </h3>
+        </div>
+    </x-slot>
+</x-app-layout>
