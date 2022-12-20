ddc8707d4d69ab94a74f729552aa7b6dba94193f
IrsalLaz <91246036+IrsalLaz@users.noreply.github.com>
2022-12-20 20:43:19 +0700

    fix ui; limit text

--- resources/views/post/detailPost.blade.php
+++ resources/views/post/detailPost.blade.php
@@ -1,7 +1,7 @@
 {{-- TODO : Add Escape Blade --}}
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+        <h2 class="font-semibold text-xl text-cyan-900 leading-tight">
             {{ __('Detail Post') }}
         </h2>
     </x-slot>
@@ -22,7 +22,8 @@
                     </div>
                 </div>
                 <div class="">
-                    <small class="text-sm text-cyan-900">{{ \Carbon\Carbon::parse($post->created_at)->diffForHumans() }}</small>
+                    <small
+                        class="text-sm text-cyan-900">{{ \Carbon\Carbon::parse($post->created_at)->diffForHumans() }}</small>
                 </div>
             </div>
 
@@ -36,37 +37,26 @@
                 {{-- Badges/Tags --}}
                 <div class="mt-4">
                     <span id="badge-dismiss-default"
-                        class="inline-flex items-center py-1 px-2 mr-2 text-sm font-medium text-blue-800 bg-blue-100 rounded dark:bg-blue-200 dark:text-blue-800">
+                        class="inline-flex items-center py-1 px-2 mr-2 text-sm font-medium text-blue-900 bg-blue-100 rounded dark:bg-blue-200 dark:text-blue-900">
                         Default
                         <button type="button"
                             class="inline-flex items-center p-0.5 ml-2 text-sm text-blue-400 bg-transparent rounded-sm hover:bg-blue-200 hover:text-blue-900 dark:hover:bg-blue-300 dark:hover:text-blue-900"
                             data-dismiss-target="#badge-dismiss-default" aria-label="Remove">
-                            <svg aria-hidden="true" class="w-3.5 h-3.5" aria-hidden="true" fill="currentColor"
-                                viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                                <path fill-rule="evenodd"
-                                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                                    clip-rule="evenodd"></path>
-                            </svg>
-                            <span class="sr-only">Remove badge</span>
                         </button>
                     </span>
                 </div>
-
-                {{-- Interaction --}}
-                <div class="mt-4 flex justify-between">
-                    <div class="flex mr-2 text-gray-700 text-sm mr-3">
-                        <i data-feather="bookmark"></i>
-                        <span>12</span>
-                    </div>
-                    <div class="inline">
-                        <div class="inline-flex mr-2 text-gray-700 text-sm mr-3">
-                            <i data-feather="message-square"></i>
-                            <span>12</span>
-                        </div>
-                        <div class="inline-flex mr-2 text-gray-700 text-sm mr-3">
-                            <i data-feather="thumbs-up"></i>
-                            <span>12</span>
-                        </div>
+                {{-- Like  --}}
+                <div class="flex justify-end">
+                    <div class="flex items-center">
+                        <form method="POST" action="{{ route('like.post', $post->id) }}">
+                            @csrf
+                            <button class="flex items-center space-x-1">
+                                <i data-feather="thumbs-up"></i>
+                                <small class="font-semibold">
+                                    {{-- {{ $post->likeCount }} --}}
+                                </small>
+                            </button>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -77,20 +67,19 @@
     <div class="max-w-4xl mx-auto sm:px-6 lg:px-8 mt-4 rounded-md">
         <form method="POST" action="" class="bg-white rounded-lg px-4 pt-2">
             <div class="flex flex-wrap -mx-3 mb-6">
-                <h2 class="px-4 pt-3 pb-2 text-gray-800 text-lg">Add a new comment</h2>
+                <h2 class="px-4 pt-3 pb-2 text-cyan-900 text-lg">Add a new comment</h2>
                 <div class="w-full md:w-full px-3 mb-2 mt-2">
                     <textarea
-                        class="bg-white rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-700 focus:outline-none focus:bg-white"
-                        name="body" placeholder='Type Your Comment' required></textarea>
+                        class="bg-white rounded border border-cyan-900 leading-normal resize-none w-full h-20 py-2 px-3 font-medium placeholder-gray-500 focus:outline-none focus:bg-white"
+                        name="body" placeholder='Type Your Comment..' required></textarea>
                 </div>
-                <div class="w-full md:w-full flex items-start md:w-full px-3">
-                    <div class="flex items-start w-1/2 text-gray-700 px-2 mr-auto">
-                        <svg fill="none" class="w-5 h-5 text-gray-600 mr-1" viewBox="0 0 24 24"
+                <div class="w-full flex items-start md:w-full px-3">
+                    <div class="flex items-start w-1/2 text-cyan-900 px-2 mr-auto">
+                        <svg fill="none" class="w-5 h-5 text-cyan-900 mr-1" viewBox="0 0 24 24"
                             stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
                         </svg>
-                        <p class="text-xs md:text-sm pt-px">Some HTML is okay.</p>
                     </div>
 
                     <div class="mt-4 -mr-1 flex justify-end">
