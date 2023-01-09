ac14ad3dcbab901854d04d587cd16180221b130a
IrsalLaz <91246036+IrsalLaz@users.noreply.github.com>
2023-01-09 10:50:31 +0700

    anya mor

--- resources/views/components/input-label.blade.php
+++
@@ -1,5 +0,0 @@
-@props(['value'])
-
-<label {{ $attributes->merge(['class' => 'block font-medium text-sm text-emerland-900']) }}>
-    {{ $value ?? $slot }}
-</label>
--- resources/views/components/primary-button.blade.php
+++
@@ -1,4 +0,0 @@
-<button
-    {{ $attributes->merge(['type' => 'submit', 'class' => 'inline-flex items-center px-4 py-2 bg-cyan-900 border border-transparent rounded-lg font-semibold text-xs text-white uppercase tracking-widest hover:bg-cyan-700 focus:bg-cyan-700 active:bg-cyan-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150']) }}>
-    {{ $slot }}
-</button>
