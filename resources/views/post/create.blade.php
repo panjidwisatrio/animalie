009e9aca6a81f49605c1c719c008f3f48c601367
panjidwisatrio <panjidwisatrio@student.telkomuniversity.ac.id>
2023-01-16 23:26:02 +0700

    fix layout

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -132,7 +132,6 @@
                                         class="add-tag-button ml-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add</button>
                                 </div>
                             </div>
-
                         </div>
 
                         <!-- Text Editor -->
